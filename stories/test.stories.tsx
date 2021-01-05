7895edd4c9cfbf97257992e2479c4026039265ba
李明阳 <1347276498@qq.com>
2021-01-05 18:49:24 +0800

    📃docs(stories): update docs

--- stories/test.stories.tsx
+++ stories/test.stories.tsx
@@ -4,36 +4,30 @@ import Captcha from 'react-captcha-code';
 export default { title: 'Basic' };
 
 export const Basic = () => {
-
   const handleChange = useCallback((captcha) => {
     console.log('captcha:', captcha);
-  }, [])
+  }, []);
 
   const captchaRef = useRef<HTMLCanvasElement>();
 
   const handleRef = (ref: any) => {
     captchaRef.current = ref.current;
-    
-  }
+  };
 
   const handleClick = () => {
     (captchaRef as any).current.click();
-  }
+  };
 
   return (
     <>
-      <Captcha
-        onRef={handleRef}
-        charNum={6}
-        onChange={handleChange}
-      />
+      <Captcha onRef={handleRef} charNum={6} onChange={handleChange} />
       <div>
         <button onClick={handleClick}>更换验证码</button>
       </div>
     </>
-  )
-}
+  );
+};
 
 Basic.story = {
   name: 'Basic',
-}
+};
